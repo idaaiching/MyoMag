2f69218377b1be52f7eab841b547b43f7ae14aba
idaaiching <ida.aichinger@gmail.com>
2018-04-29 10:18:35 +0200

    reorganized main()

--- src/main.c
+++ src/main.c
@@ -1,39 +1,52 @@
-// main.c
-
+// main.c 
+// The MyoMag program reads in a signal data and 
+// process it to return its magnitude 
+// at each time step.
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "acc.h"  /* Include the header here, to obtain the function declaration */
 
 
 
 int main(int argc, char* argv[])
 {
+	clock_t start_t, end_t;
 
+	int n = 7024; // number of lines in the csv file
 	struct accelerometer acc_data[7024];
+	double magnitude_array[7024] = {0};
 	char filepath[20];
 	strcpy(filepath, "src/AccData.csv");
 	int error = 0;
-	error = readCSV(filepath, acc_data); 
-	if (error == 1)
-		printf("error: %d\n", error);
-	printf("here is the first time: ");
-	printf("%g\n", acc_data[1].x);
-	printf("here is the last time: ");
-	printf("%d\n", acc_data[2].t);
+	int n_repetition = 10;
+	start_t = clock();
+	for(int i = 0; i < n_repetition; i++)
+	{
+		error = readCSV(filepath, acc_data); 
+		if (error == 1) printf("error: %d\n", error);
+		calculateMagnitude(acc_data, magnitude_array, n);
+	}
+	end_t = clock();
 
-	struct accelerometer instance[1] = {
-		{.t = 10, .x=6., .y=3., .z=2.}
-	};
-	printf("here is the last time: ");
-	printf("%d\n", instance[0].t);
-	
-	double magnitude_array[7024];
-	calculateMagnitude(acc_data, magnitude_array, 7024);
-	printf("length is %lu\n", sizeof(acc_data)/sizeof(acc_data[0]));
-	printf("magnitude is %g\n", magnitude_array[2]);
-	printf("%g\n", magnitude_array[0]);
-	printf("%g\n", magnitude_array[1]);
+	printf("The magnitude has been calculated an saved in magnetude_array.\n"
+		"The first three lines are plotted here "
+		"in the format (time, magnitude): \n"
+		"%d: %g\n"
+		"%d: %g\n"
+		"%d: %g\n",
+		acc_data[0].t, magnitude_array[0],
+		acc_data[1].t, magnitude_array[1],
+		acc_data[2].t, magnitude_array[2]
+		);
+
+	printf("Total time taken by CPU for %d repetition: %.2f seconds\n", 
+		n_repetition,
+		(double)(end_t - start_t) / CLOCKS_PER_SEC );
+
+	printf("Done.\n");
 
     return 0;
 }
